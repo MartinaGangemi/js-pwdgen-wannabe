6bfbc6f3ccb64091a020cf50b4bc894a21bdfae8
MartinaGangemi <martinagangemi94@gmail.com>
2022-03-23 15:08:13 +0100

    Initial Commit

---
+++ main.js
@@ -0,0 +1,14 @@
+
+const firstname = prompt("Qual è il tuo nome?")
+console.log(firstname)
+
+const lastname = prompt("Qual è il tuo cognome?")
+console.log(lastname)
+
+const color = prompt("Qual è il tuo colore preferito?")
+console.log(color)
+
+const number = 22
+console.log(number)
+
+document.getElementById("password").innerHTML = firstname + lastname + color + number
